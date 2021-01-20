1a700384855f90674d4556728e7b888f25f2d763
kinjal <kapatel190@gmail.com>
2021-01-20 17:46:15 +0530

    Updates

---
+++ static/js/main.52874435.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{4:function(e,t,r){"use strict";r.r(t);var c=r(2),n=(r(1),r(3)),p=r.n(n),a=Object(c.jsx)("h1",{children:"Hello World"});p.a.render(a,document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.52874435.chunk.js.map
